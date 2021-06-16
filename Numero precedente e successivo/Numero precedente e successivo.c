11b86788864480a816b29d974903270c78db252f
Massimo931 <massimoarnoni@gmail.com>
2021-06-16 13:17:54 +0200

    Numero precedente e successivo

---
+++ Numero precedente e successivo/Numero precedente e successivo.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+main()
+{
+	int j;
+	
+	printf("Inserisci un valore: ");
+	scanf("%d",&j);
+	
+	j--;
+	printf("\n%d",j);
+	
+	j++;
+	printf("\n%d",j);
+	
+	j++;
+	printf("\n%d",j);
+	
+	system("pause>null");
+}
+
